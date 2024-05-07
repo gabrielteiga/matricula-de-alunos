fd250462fc7590a1a999c9a73e230b63ad420e35
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-05-07 13:25:24 -0300

    Criado repository em memória.

---
+++ src/repository/aluno/AlunoRepositoryMem.php
@@ -0,0 +1,46 @@
+<?php
+    namespace Gabriel\php\Repository\Aluno;
+
+    use Gabriel\php\Domain\Aluno\Aluno;
+    use Gabriel\php\Domain\Aluno\AlunoNaoEncontrado;
+    use Gabriel\php\Domain\Aluno\AlunoRepository;
+    use Gabriel\php\Domain\Cpf;
+
+    class AlunoRepositoryMem implements AlunoRepository
+    {
+        private array $alunos = [];
+
+        public function __construct(array $alunos)
+        {
+            $this->alunos = $alunos;
+        }
+
+        public function addAluno(Aluno $aluno): void
+        {
+            $this->alunos[] = $aluno;
+        }
+
+        public function buscarPorCpf(Cpf $cpf): Aluno
+        {
+            $alunosFiltrados = array_filter($this->alunos, 
+                fn(Aluno $aluno) => $aluno->getCpf() == $cpf);
+
+            if (count($alunosFiltrados) === 0) 
+            {
+                throw new AlunoNaoEncontrado($cpf);
+            }
+
+            if (count($alunosFiltrados) > 1)
+            {
+                throw new \Exception("Mais de um aluno com cpf! FATAL!");
+            }
+
+            return $alunosFiltrados[0];
+        }
+
+        /** @return Aluno[] */
+        public function getAllAlunos(): array
+        {
+            return $this->alunos;
+        }
+    }
